6e516ddc86091e4bc86982e1f9b75f55fecea9de
oleh <phd.chornyi@gmail.com>
2024-10-22 11:38:27 +0300

    2nd: header

--- public/main.js
+++ public/main.js
@@ -13,3 +13,28 @@ navLinks.addEventListener("click", (e)=>{
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const scrollRevealOption = {
+    distance:"50px",
+    origin:"bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__container img", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__container p", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
